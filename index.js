ceed957a956a4ac8d21ae11abdcde631b755abc3
unknown <joaquincerruti54@gmail.com>
2021-11-17 11:01:58 -0300

    anotaciones

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const ulEl = document.getElementById("ul-el")
 
 
 //asi solo se manipula el dom un a vez
+
+//la función tiene un let con valor vacio y una cuenta FOR, esta cuenta usa myleads como limite y es usada para modificar listitmes cada vez que es llamada
+//a su vez,listitems tien un html envuelto para darle formato de lista de links,luego, el ulEl cambia su innerhtml para ser el de listitems y asi renderizar la lista deseaa
+
 function renderlist() {
     let listitems = ""
     for (let i = 0; i < myleads.length; i++) {
@@ -16,11 +20,14 @@ function renderlist() {
     console.log(listitems)
 }
 
+
+//este eventlistener le da un valor vacio al input,esto permite al usuario editarlo como le parezca tras lo cual el mismo pushea ese valor editado a myleads cada vez que se hace click en SAVE
+//el valor es agregado a myleads y la funcion listitems es llamada, al tener un += en el for loop esta renderiza myleads con el ultimo valor agregado por el eventlistener sin borrar el anterior
 saveEl.addEventListener("click", function () {
 
+    inputEl.value = ""
     myleads.push(inputEl.value)
     console.log(myleads)
     renderlist()
-    inputEl.value = ""
     
 })
