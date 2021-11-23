2c44679e6457085bcec10b0c2bd67ee4dbcc1256
unknown <joaquincerruti54@gmail.com>
2021-11-23 12:32:56 -0300

    boton borrar

--- index.js
+++ index.js
@@ -1,33 +1,63 @@
 let myleads = []
+let historial = []
 const inputEl = document.getElementById("input-el")
 const saveEl = document.getElementById("save-btn")
+const deleteEl = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+const historialEl = document.getElementById("historial-btn")
+
+
+
+saveEl.addEventListener("click", function () {
+
+    myleads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myleads", JSON.stringify(myleads))
+    localStorage.setItem("history", JSON.stringify(historial))
+    renderlist()
+
+})
+
+deleteEl.addEventListener("dblclick", function () {
+
+    localStorage.removeItem("myleads")
+    myleads = []
+    renderlist()
+
+})
 
 
 
 
-//asi solo se manipula el dom un a vez
+const savedLeads = JSON.parse(localStorage.getItem("myleads"))
+if (savedLeads){
+    myleads = savedLeads
+}
 
-//la función tiene un let con valor vacio y una cuenta FOR, esta cuenta usa myleads como limite y es usada para modificar listitmes cada vez que es llamada
-//a su vez,listitems tien un html envuelto para darle formato de lista de links,luego, el ulEl cambia su innerhtml para ser el de listitems y asi renderizar la lista deseaa
+console.log(savedLeads)
+
+if (savedLeads) {
+    myleads = savedLeads
+    renderlist()
+}
+
 
 function renderlist() {
     let listitems = ""
     for (let i = 0; i < myleads.length; i++) {
-        listitems +=   "<li> <a target ='_blank' href='" + myleads[i] + "'>" + myleads[i] + " </a>  </li>"
+
+        listitems += `
+<li>
+
+<a target='_blank' href='${myleads[i]}'> 
+${myleads[i]}
+</a>
+
+</li>
+`
+
     }
     ulEl.innerHTML = listitems
-    console.log(listitems)
+
 }
 
-
-//este eventlistener le da un valor vacio al input,esto permite al usuario editarlo como le parezca tras lo cual el mismo pushea ese valor editado a myleads cada vez que se hace click en SAVE
-//el valor es agregado a myleads y la funcion listitems es llamada, al tener un += en el for loop esta renderiza myleads con el ultimo valor agregado por el eventlistener sin borrar el anterior
-saveEl.addEventListener("click", function () {
-
-    inputEl.value = ""
-    myleads.push(inputEl.value)
-    console.log(myleads)
-    renderlist()
-    
-})
